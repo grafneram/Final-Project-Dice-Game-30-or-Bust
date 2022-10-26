9fc9f8637147950ff8b93fcf460003d223eb867b
grafneram <crashly0302@gmail.com>
2022-10-25 23:54:22 -0600

    First push

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.*;
+
 class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        class DiceGame {
+        class Player {
             private String name = "unknown";
             private int score = 0;
 
-            public DiceGame(String setName, int setScore) { //constructor
+            public Player(String setName, int setScore) { //constructor
                 this.name = setName; //explicit setters
                 this.score = setScore; //explicit setters
             }
@@ -29,49 +30,64 @@ class Main {
                 this.score = score;
             }
 
+            public void addScore(int addScore) {
+             score = getScore() + addScore; //sets score to
+            }
+
 
             public void askName() {
                 System.out.println("\nWhat is your name, Player: ");
                 name = input.next();
                 System.out.println("Welcome to the game, " + getName());
             }
+        }
+
+        class DiceGame {
+            Player[] playerNames; //array of players (defined in main)
+            public DiceGame(Player[] players) { //will run through each player
+                this.playerNames = players;
+            }
 
             public void display() {
-                while (score <= 30) {
-                    int diceRoll1 = (int) (Math.random() * 6) + 1; //dice 1
-                    int diceRoll2 = (int) (Math.random() * 6) + 1; //dice 2
-                    int diceRollTotal = diceRoll1 + diceRoll2; // total of dice 1 and dice 2
+                boolean gameInProgress = true;
 
+                while (gameInProgress ) { //will run while true:
+                    for (Player player : playerNames) { //runs for each player, think of it as switching turns
+                        int diceRoll1 = (int) (Math.random() * 6) + 1; //dice 1
+                        int diceRoll2 = (int) (Math.random() * 6) + 1; //dice 2
+                        int diceRollTotal = diceRoll1 + diceRoll2; // total of dice 1 and dice 2
 
-                    System.out.println("\nPlayer: " + getName() + ".... Your total score is: " + getScore()); //says player name and score
-                    System.out.println("First dice is: " + diceRoll1); //prints dice 1
-                    System.out.println("Second dice is: " + diceRoll2);// prints dice 2
-                    System.out.println("The total of the two dice is: " + diceRollTotal); //prints total of dice1+ dice 2
+                        System.out.println("\nPlayer: " + player.getName() + ".... Your total score is: " + player.getScore()); //says player name and score
+                        System.out.println("First dice is: " + diceRoll1); //prints dice 1
+                        System.out.println("Second dice is: " + diceRoll2);// prints dice 2
+                        System.out.println("The total of the two dice is: " + diceRollTotal); //prints total of dice1+ dice 2
 
-                    System.out.println("Do you want to: 1.Keep dice1, 2.Keep dice2, 3.Keep the total of dice? (Please type 1 or 2 or 3): ");
-                    String user_input = input.nextLine();
-                    while (!user_input.equals("1") && !user_input.equals("2") && !user_input.equals("3")) { //if user does not enter 1,2 or 3:
-                        System.out.println("Please choose 1, 2 or 3");
-                        user_input = input.nextLine(); //loop until user_input = 1,2 or 3
-                    }
-                    if (user_input.equals("1")) {
-                        score = score + diceRoll1; //1= dice1
-                    }
-                    if (user_input.equals("2")) { //2 = dice2
-                        score = score + diceRoll2;
-                    }
-                    if (user_input.equals("3")) { //3 = dice1+dice2
-                        score = score + diceRollTotal;
-                    }
-                    System.out.println("Your new total score is: " + score); //states score based on user_input
+                        System.out.println("Do you want to: 1.Keep dice1, 2.Keep dice2, 3.Keep the total of dice? (Please type 1 or 2 or 3): ");
+                        String user_input = input.nextLine();
+                        while (!user_input.equals("1") && !user_input.equals("2") && !user_input.equals("3")) { //if user does not enter 1,2 or 3:
+                            System.out.println("Please choose 1, 2 or 3");
+                            user_input = input.nextLine(); //loop until user_input = 1,2 or 3
+                        }
+                        if (user_input.equals("1")) {//1= dice1
+                            player.addScore(diceRoll1); //(score = getScore() + addScore) +Dice1
+                        }
+                        if (user_input.equals("2")) { //2 = dice2
+                            player.addScore(diceRoll2);//(score = getScore() + addScore) +Dice2
+                        }
+                        if (user_input.equals("3")) { //3 = dice1+dice2
+                            player.addScore(diceRollTotal); //(score = getScore() + addScore) +DiceTotal
+                        }
+                        System.out.println("Your new total score is: " + player.getScore()); //states score based on user_input
 
-                    if (score > 30) {
-                        score = 0;
-                        System.out.println("\nOh no! Your score is over 30. Your score is now reset back to 0.");
-                    }
-                    if (score == 30) {
-                        System.out.println("Your score is 30! Congratulations " + getName() + " you have won!");
-                        break; //quits out of our code, because a player won
+                        if (player.getScore() > 30) {
+                            player.setScore(0);
+                            System.out.println("Oh no! Your score is over 30. Your score is now reset back to 0.");
+                        }
+                        if (player.getScore() == 30) {
+                            System.out.println("Your score is 30! Congratulations, Player: " + player.getName() + " you have won!");
+                            gameInProgress = false; //will stop our gameInProgress loop because false
+                            break; //closes out of loop
+                        }
                     }
                 }
             }
@@ -87,15 +103,15 @@ class Main {
 
         System.out.println("\nLets start the game!");
 
-        DiceGame[] arr;
-        arr = new DiceGame[2];
-        arr[0] = new DiceGame("unknown",0);
-        arr[1] = new DiceGame("unknown",0);
+        Player[] arr;
+        arr = new Player[2];
+        arr[0] = new Player("unknown", 0);
+        arr[1] = new Player("unknown", 0);
 
-        arr[0].askName();
-        arr[1].askName();
+        DiceGame diceGame = new DiceGame(arr);
 
-        arr[0].display();
-        arr[1].display();
+        arr[0].askName(); //asks for player 1 name
+        arr[1].askName(); //asks for player 2 name
+        diceGame.display(); //prints our actual game
     }
 }
